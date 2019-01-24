d20df52b85dcb2e8590b9d6b459116c620afc38a
Shashking <Shashankk165@gmail.com>
2019-01-24 11:20:55 +0530

    Ruby Test and Postgresql COmmands add

---
+++ Test1.rb
@@ -0,0 +1,18 @@
+END{
+	puts "End of code \n" 
+}
+data={"Shashank"=> "Computer Science","Akash"=>"Web-Design","Prince"=>"Graphic Design"}
+puts data["Shashank"]
+puts data["Akash"]
+puts data["Prince"]
+
+data=["Arpan","Alok","Rohit"]
+puts data[0]
+puts data[1]
+puts data[2]
+
+puts "Shashank"+" Kumar"
+
+BEGIN{
+	puts "Begin of code"
+}
