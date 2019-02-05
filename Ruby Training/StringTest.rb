02d5661ec5f6a3c4e50b1b6847481bb3af561f32
ShashKing <Shashankk165@gmail.com>
2019-02-05 18:42:20 +0530

    String files added

---
+++ Ruby Training/StringTest.rb
@@ -0,0 +1,12 @@
+mystr=String.new("This is Test")
+foo=mystr.downcase
+puts "#{foo}"
+puts "#{mystr.length}"
+puts "#{mystr.empty?}"
+puts "#{mystr.count('s')}"
+puts "#{mystr.insert(3,'his')}"
+puts "#{mystr.swapcase}"
+puts "#{mystr.capitalize}"
+puts "#{mystr.split}"
+puts "#{mystr.strip}"
+puts "#{mystr.to_i}"
---
+++ Ruby Training/StringTest3.rb
@@ -0,0 +1,7 @@
+x="Ruby"
+print x.upcase
+puts " "
+print x.downcase
+puts " "
+print x.capitalize
+puts " "
