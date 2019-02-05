2ddbb7f477d823cad61f9ebd983e2bf3aec8d573
ShashKing <Shashankk165@gmail.com>
2019-02-05 13:07:13 +0530

    Operator file added

---
+++ Ruby Training/Operator.rb
@@ -0,0 +1,40 @@
+a,b,c,e=10,20,2,2
+ba=60
+bb=13
+#assume bitwise number
+puts a+b
+puts b-a
+puts a*b
+puts b/a
+puts b%a
+puts b**c
+puts a==b
+puts a!=b
+puts a<b
+puts a>b
+puts a<=b
+puts a>=b
+puts a<=>b
+puts a.eql?b
+puts a.equal?b
+d=a+b
+puts d
+d+=b
+puts d
+d-=b
+puts d
+d/=b
+puts d
+d%=b
+puts d
+
+e**=c
+puts d
+#bitwise operators
+puts ba&bb
+puts ba|bb
+puts ba^bb
+puts (~ba)
+puts (~bb)
+puts ba<<2
+puts 2>>bb
