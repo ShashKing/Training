b6fa0b97c1c10d92f98c57a7787056e5fede746f
ShashKing <Shashankk165@gmail.com>
2019-02-05 12:36:53 +0530

    Ruby Files are added

---
+++ Ruby Training/Class.rb
@@ -0,0 +1,7 @@
+class Class 
+	def show
+		puts "This is Show function"
+	end
+end
+object=Class.new
+object.show
---
+++ Ruby Training/Syntax.rb
@@ -0,0 +1,16 @@
+puts "Hello world"
+#This is a comment in ruby
+a=50
+b=40
+puts "Sum is #{a+b}"
+END{
+	puts "This is end block"
+}
+BEGIN{
+	puts "This is begin block"
+}
+print "Hello World This is a print method in ruby"
+print "Again a print method"
+#in print method print string in same line whereas in puts method it automatically print string in next line
+puts "Hello puts here"
+puts "This is the difference between print and puts"
