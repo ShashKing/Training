5818821778f7cb2c82b17c1832197eee2d096068
ShashKing <Shashankk165@gmail.com>
2019-02-06 17:50:14 +0530

    Range in Ruby files added in git

---
+++ Ruby Training/RangeTest2.rb
@@ -0,0 +1,9 @@
+range1=(1..10).to_a
+#to_a This method convert object into array 
+puts range1
+range2=('baq'..'bav').to_a
+puts range2
+
+string="My name is shashnk"
+puts string.split('n')
+#This method split string 
---
+++ Ruby Training/RangeTest4.rb
@@ -0,0 +1,12 @@
+if ((1..10)===5)
+	puts "5 lies between 1 and 10"
+end
+
+if (('a'..'j')==='c')
+	puts "c lies between a and j"
+	
+end
+if (('a'..'j')==='z')
+	puts "z lies between a and j"
+	
+end
