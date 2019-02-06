0341eed85825fd919aaa3a5e7842cdde66420083
ShashKing <Shashankk165@gmail.com>
2019-02-06 15:38:28 +0530

    String files added

---
+++ Ruby Training/StringTest4.rb
@@ -0,0 +1,6 @@
+puts "Enter the string value"
+string=gets.chomp.to_s
+print string[-1]
+print string[1...-1]
+print string[0]
+puts " "
---
+++ Ruby Training/StringTest5.rb
@@ -0,0 +1,7 @@
+a="a"
+for i in 1..5
+	puts a
+	a=a+"a"
+
+end
+print i.length
