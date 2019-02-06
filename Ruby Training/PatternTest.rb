f90c3e323edc2487159bcd4fd59860e88c3a360d
ShashKing <Shashankk165@gmail.com>
2019-02-06 18:30:10 +0530

    Pattern program added

---
+++ Ruby Training/PatternTest.rb
@@ -0,0 +1,25 @@
+n=5
+k=1
+for i in 0...5
+	
+		print "*"*n
+		print " "*(k-1)
+		print " "*(k-1)
+		print "*"*n
+		print "\n"
+		n-=1
+		k+=1
+
+end
+m=1
+l=5
+for j in 0...5
+	
+		print "*"*m
+		print " "*(l-1)
+		print " "*(l-1)
+		print "*"*m
+		print "\n"
+		m+=1
+		l-=1
+end
