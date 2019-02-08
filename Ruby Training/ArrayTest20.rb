6ed775054a0c787facd63723100ced7b5c698011
ShashKing <Shashankk165@gmail.com>
2019-02-08 15:10:09 +0530

    Array files added

---
+++ Ruby Training/ArrayTest20.rb
@@ -0,0 +1,7 @@
+a=["a","b","c","d"]
+print a.pop
+puts " "
+print a.pop(2)
+puts " "
+print a
+puts " "
---
+++ Ruby Training/ArrayTest21.rb
@@ -0,0 +1,17 @@
+arr=[1,2,3].product([4,5])
+brr=[1,2].product([1,2])
+crr=[1,2].product([3,4],[5,6])
+
+drr=[1,2].product()
+err=[1,2].product([])
+
+print arr
+puts " "
+print brr
+puts " "
+print crr
+puts " "
+print drr
+puts " "
+print err
+puts " "
