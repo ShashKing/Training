209ea88edb2a5b50cbefc355e1163f4780e68188
ShashKing <Shashankk165@gmail.com>
2019-02-08 11:32:20 +0530

    Array files added

---
+++ Ruby Training/ArrayTest12.rb
@@ -0,0 +1,18 @@
+a=[1,2,3,4]
+print a.combination(1).to_a
+puts " "
+print a.combination(2).to_a
+puts " "
+print a.combination(3).to_a
+puts " "
+print a.combination(4).to_a
+puts " "
+print a.combination(5).to_a
+puts " "
+print  a.combination(6).to_a
+puts " "
+
+
+brr=["a",nil,"b",nil,"c",nil].compact
+print brr
+puts " "
---
+++ Ruby Training/ArrayTest15.rb
@@ -0,0 +1,5 @@
+a=[1,3,4,5,6,7,8,2]
+#print a.drop(3)
+#puts " "
+print a.drop_while{|i|i<3}
+puts " "
