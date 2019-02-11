89a3f1ef27ad7cb4ed85e7e530d0e2e7b680f149
ShashKing <Shashankk165@gmail.com>
2019-02-11 17:11:12 +0530

    Files added

---
+++ Ruby Training/ArraySortTest.rb
@@ -0,0 +1,16 @@
+print num=[25,45,65,85,77,13,02,85,55]
+sort=num.sort
+puts
+print sort
+puts 
+puts sort.first
+puts sort.last
+
+print str="ABC BCD AAB AAA BBA"
+str=str.split(" ")
+puts 
+sortstring= str.sort
+print sortstring
+puts
+puts sortstring.first
+puts sortstring.last
