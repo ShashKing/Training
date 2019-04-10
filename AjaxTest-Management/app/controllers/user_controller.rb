b0d9a32ab3b2ada65d39120d8435544a89ee89b3
ShashKing <Shashankk165@gmail.com>
2019-04-10 11:09:15 +0530

    Sorting Fixed

--- AjaxTest-Management/app/controllers/user_controller.rb
+++ AjaxTest-Management/app/controllers/user_controller.rb
@@ -1,18 +1,11 @@
 class UserController < ApplicationController
 
   def index
-    
     @users = "https://jsonplaceholder.typicode.com/users"
     @response = HTTParty.get(@users)
     @res = JSON.parse(@response.body)
-    debugger
-    #@res = @res.sort_by{|x| x['name'] }
-		#@res = @res.reverse if sort_order == 'desc'
-		#@res = @res.paginate(:page => params[:page], :per_page => 5)
+		
     params[:type]
-
-
-    
     @user = params[:user] if params[:user].present?
     @name = params[:user][:name] if params[:user].present?
     @country = params[:user][:country] if params[:user].present?
@@ -29,10 +22,17 @@ class UserController < ApplicationController
     params[:sort_cat] ||='name'
     params[:sort_type] ||= 'asc'
     params[:page] ||=1
+    sort_cat = params[:sort_cat]
+    sort_type = params[:sort_type]
+
     unless params[:search_str].blank?
       @res = @res.where("name like ? ","%#{params[:search_str]}%").order("#{params[:sort_cat]} #{params[:sort_type]}").paginate(page: params[:page], per_page: 2)
     else
-      #@res = @res.paginate(page: params[:page], per_page: 2).order("#{params[:sort_cat]} #{params[:sort_type]}").paginate(page: params[:page], per_page: 2)
+      @res = @res.sort_by{|x| x[sort_cat] }
+      @res = @res.reverse if sort_type == 'desc'
+      #@res = @res.paginate(:page => params[:page], :per_page => 5)
+      # @res = @res.order("#{params[:sort_cat]} #{params[:sort_type]}").paginate(page: params[:page], per_page: 2)
     end
+    
   end
 end
